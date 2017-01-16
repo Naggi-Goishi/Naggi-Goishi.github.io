bec408dc96504d56ecac445a641b4248b60b61f8
Naggi-Goishi <naggitapdance@gmail.com>
2017-01-15 21:02:16 -0800

    change color

--- _apps/shobodot/app.js
+++ _apps/shobodot/app.js
@@ -1,12 +1,12 @@
 window.onload = function(){
   const shobodot = new ShoboDot({colorBoxes: true, clickFunction: true});
+  const downloadDots = [];
+  document.getElementById(ShoboDot.getXline() * (ShoboDot.maxYDots - 1) + 1).click();
 };
 
 window.addEventListener('keydown', function(e){
   if (e.keyCode === 91){
     command = true;
-  } else if (e.keyCode == 18){
-    rgb.max -= 10;
   }
 });
 
